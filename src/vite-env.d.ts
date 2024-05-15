7eca1eaf4be088743f937adecd7c73e5f93fbda1
Russell Oakham <russell.oakham@and.digital>
2024-05-15 21:39:53 +0100

    fixed linting issues

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+	readonly VITE_APP_TITLE: string
+	readonly VITE_AWS_USER_POOL_ID: string
+	readonly VITE_AWS_USER_POOL_CLIENT_ID: string
+	// more env variables...
+}
+
+interface ImportMeta {
+	readonly env: ImportMetaEnv
+}
